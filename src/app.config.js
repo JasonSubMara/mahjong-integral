9d94306e9aa22f704d1b0007ee9ebdc0ec905ebe
JasonSubmara <jie.yr127@gmail.com>
2024-01-08 16:44:10 +0800

    feat: project init

---
+++ src/app.config.js
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  pages: [
+    'pages/index/index',
+    'pages/mine/index',
+    'pages/rules/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  },
+})
